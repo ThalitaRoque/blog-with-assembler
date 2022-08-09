5c1f4488aa72a079199edb0d5d8c078edf6fea40
David Moreno <cesardaviddev@gmail.com>
2022-08-09 18:58:17 +0200

    Nuevos

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const catContainer = document.getElementById("catContainer");
 btnCat.addEventListener("click", getRandomCat);
 
 function getRandomCat() {
-  fetch("http://localhost:3000")
-    .then((res) => res.json())
-    .then((data) => {
-      catContainer.innerHTML = `<img src=${data.file} alt="cat" />`;
+  fetch("http://localhost:3000/posts")
+    .then((array) => array.json())
+    .then((arrayObjetos) => {
+      console.log(arrayObjetos[5].body);
     });
 }
