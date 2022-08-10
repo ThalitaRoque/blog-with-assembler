d97b7cab7bfdb3d2d731d2ef05518109fdc9a5b3
David Moreno <cesardaviddev@gmail.com>
2022-08-10 14:06:29 +0200

    step3 Easy

--- script.js
+++ script.js
@@ -4,13 +4,9 @@ const catContainer = document.getElementById("catContainer");
 
 //  for(i=0; i<posts.length; i++){
 //   posts[i].addEventListener("click", ()=>{
-    
+
 //   })
 //  }
-
-
-
-
 
 window.addEventListener("load", () => {
   fetch("http://localhost:3000/posts")
@@ -50,7 +46,6 @@ window.addEventListener("load", () => {
         const modalTitle9 = document.getElementById("title-modal9");
         modalTitle9.textContent = "Title: " + arrayObjetos[8].title;
 
-
         const modalBody = document.getElementById("body-modal1");
         modalBody.textContent = "Body: " + arrayObjetos[0].body;
 
@@ -77,15 +72,76 @@ window.addEventListener("load", () => {
 
         const modalBody9 = document.getElementById("body-modal9");
         modalBody9.textContent = "Body: " + arrayObjetos[8].body;
-
       }
+    });
+
+  fetch("http://localhost:3000/users")
+    .then((array) => array.json())
+    .then((arrayObjetos) => {
+      // console.log(arrayObjetos[0].name);
+      // console.log(arrayObjetos[0].email);
+
+      arrayObjetos.map((copy) => {
+        console.log(`Nombre ${copy.name} y Email ${copy.email}`);
+
+        const modalName = document.getElementById("name-modal1");
+        modalName.textContent = "name: " + arrayObjetos[0].name;
+
+        const modalName2 = document.getElementById("name-modal2");
+        modalName2.textContent = "name: " + arrayObjetos[1].name;
+
+        const modalName3 = document.getElementById("name-modal3");
+        modalName3.textContent = "name: " + arrayObjetos[2].name;
+
+        const modalName4 = document.getElementById("name-modal4");
+        modalName4.textContent = "name: " + arrayObjetos[3].name;
+
+        const modalName5 = document.getElementById("name-modal5");
+        modalName5.textContent = "name: " + arrayObjetos[4].name;
+
+        const modalName6 = document.getElementById("name-modal6");
+        modalName6.textContent = "name: " + arrayObjetos[5].name;
+
+        const modalName7 = document.getElementById("name-modal7");
+        modalName7.textContent = "name: " + arrayObjetos[6].name;
+
+        const modalName8 = document.getElementById("name-modal8");
+        modalName8.textContent = "name: " + arrayObjetos[7].name;
+
+        const modalName9 = document.getElementById("name-modal9");
+        modalName9.textContent = "name: " + arrayObjetos[8].name;
+
+        const modalEmail = document.getElementById("email-modal1");
+        modalEmail.textContent = "email: " + arrayObjetos[0].email;
+
+        const modalEmail2 = document.getElementById("email-modal2");
+        modalEmail2.textContent = "email: " + arrayObjetos[1].email;
+
+        const modalEmail3 = document.getElementById("email-modal3");
+        modalEmail3.textContent = "email: " + arrayObjetos[2].email;
+
+        const modalEmail4 = document.getElementById("email-modal4");
+        modalEmail4.textContent = "email: " + arrayObjetos[3].email;
+
+        const modalEmail5 = document.getElementById("email-modal5");
+        modalEmail5.textContent = "email: " + arrayObjetos[4].email;
+
+        const modalEmail6 = document.getElementById("email-modal6");
+        modalEmail6.textContent = "email: " + arrayObjetos[5].email;
+
+        const modalEmail7 = document.getElementById("email-modal7");
+        modalEmail7.textContent = "email: " + arrayObjetos[6].email;
+
+        const modalEmail8 = document.getElementById("email-modal8");
+        modalEmail8.textContent = "email: " + arrayObjetos[7].email;
+
+        const modalEmail9 = document.getElementById("email-modal9");
+        modalEmail9.textContent = "email: " + arrayObjetos[8].email;
+      });
     });
 });
 
-
-
-
-
+/* PRUEBA */
 
 btnCat.addEventListener("click", getRandomCat);
 
